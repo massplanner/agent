d5cf6d7a25bc665a47d87875934d3e3632142c58
Christopher Bradley <declarationcb@gmail.com>
2023-09-10 12:00:29 -0400

    updated installation script

--- mpagent/install_script.sh
+++ mpagent/install_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define variables
-PROJECT_NAME="massplanner"
+PROJECT_NAME="mpagent"
 DOWNLOAD_URL="http://github.com/massplanner/agent/zipball/master/"  # Replace with your project's download URL
 
 # Create a temporary directory for the project
@@ -14,7 +14,7 @@ curl -L -o $TEMP_DIR/$PROJECT_NAME.zip $DOWNLOAD_URL
 unzip -q $TEMP_DIR/$PROJECT_NAME.zip -d $TEMP_DIR
 
 # Find the directory containing your Python program
-PROGRAM_DIR=$(find $TEMP_DIR -type d -name "massplanner-agent*" | head -n 1)
+PROGRAM_DIR=$(find $TEMP_DIR -type d -name "$PROJECT_NAME*" | head -n 1)
 
 if [ -z "$PROGRAM_DIR" ]; then
   echo "Error: Could not locate the program directory."
@@ -41,7 +41,7 @@ python3.9 -m pipenv shell
 pipenv install -r requirements.txt  # If you have a requirements.txt file
 
 # Provide instructions for running the program
-echo "Installation complete. You can now run your program using 'python massplanner.py'"
+echo "Installation complete. You can now run your program using 'python massplanner.py"
 
 # Deactivate the virtual environment when done
 deactivate

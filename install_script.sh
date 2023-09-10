a16d4a42b7e705bec34bbbf64f38d1cf7ac055e7
Christopher Bradley <declarationcb@gmail.com>
2023-09-10 17:25:08 -0400

    updated installation script

--- install_script.sh
+++ install_script.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 # Define variables
-PROJECT_NAME="massplanner"
+PROJECT_NAME="massplanner-agent"
 DOWNLOAD_URL="http://github.com/massplanner/agent/zipball/master/"  # Replace with your project's download URL
 
 # Create a temporary directory for the project
 TEMP_DIR=$(mktemp -d)
 
 # Download the project's source code or distribution
-curl -L -o $TEMP_DIR/$PROJECT_NAME.zip $DOWNLOAD_URL
+curl -L -o $PROJECT_NAME.zip $DOWNLOAD_URL
 
-# Unzip the downloaded file to the temporary directory
-unzip -q $TEMP_DIR/$PROJECT_NAME.zip -d $TEMP_DIR
+# Unzip the downloaded file to the temporary directory and rename to the project name
+unzip $PROJECT_NAME.zip -d $TEMP_DIR
+
+# Remove the downloaded file
+rm $PROJECT_NAME.zip
 
 # Find the directory containing your Python program
 PROGRAM_DIR=$(find $TEMP_DIR -type d -name "massplanner-agent*" | head -n 1)
@@ -34,14 +37,14 @@ cd $PROJECT_NAME  # This should be the current directory now
 # Create a virtual environment for Python 3.9 and install project dependencies
 python3.9 -m pipenv --python 3.9 install
 
+# Install the dependencies from the Pipfile.lock file
+python3.9 -m pipenv sync
+
 # Activate the virtual environment
 python3.9 -m pipenv shell
 
-# Install project dependencies (if any)
-pipenv install -r requirements.txt  # If you have a requirements.txt file
-
 # Provide instructions for running the program
-echo "Installation complete. You can now run your program using 'python massplanner.py'"
+echo "Installation complete. You can now run your program using 'python massplanner-agent/executable.py'"
 
 # Deactivate the virtual environment when done
-exit
+deactivate

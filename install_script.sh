713b1f719f5a697a47273239cdddbcac3908528c
Christopher Bradley <declarationcb@gmail.com>
2023-09-10 11:36:41 -0400

    updated installation script

--- install_script.sh
+++ install_script.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Define variables
-PROJECT_NAME="massplanner"
-VENV_NAME="venv"
+PROJECT_NAME="massplanner_local"
 DOWNLOAD_URL="http://github.com/massplanner/agent/zipball/master/"  # Replace with your project's download URL
 
 # Create a directory for the project
@@ -15,17 +14,17 @@ curl -L -o $PROJECT_NAME.zip $DOWNLOAD_URL
 # Unzip the downloaded file (if it's a zip file)
 unzip -q $PROJECT_NAME.zip
 
-# Create a virtual environment
-python3 -m venv $VENV_NAME
+# Install pipenv for Python 3.9
+python3.9 -m pip install pipenv
+
+# Create a virtual environment for Python 3.9 and install project dependencies
+python3.9 -m pipenv --python 3.9 install
 
 # Activate the virtual environment
-source $VENV_NAME/bin/activate
-
-# Install project dependencies (if any)
-pip install -r requirements.txt  # If you have a requirements.txt file
+python3.9 -m pipenv shell
 
 # Provide instructions for running the program
 echo "Installation complete. You can now run your program using 'python main.py'."
 
-# Deactivate the virtual environment when done
-deactivate
+# Exit the virtual environment when done
+exit

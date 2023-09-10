3b34829ceaa67d4728945849e9ea6e621837debd
Christopher Bradley <declarationcb@gmail.com>
2023-09-10 11:55:53 -0400

    updated installation script

--- install_script.sh
+++ install_script.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
 # Define variables
-PROJECT_NAME="massplanner_local"
+PROJECT_NAME="massplanner-agent"
 DOWNLOAD_URL="http://github.com/massplanner/agent/zipball/master/"  # Replace with your project's download URL
 
-# Create a directory for the project
-mkdir -p $PROJECT_NAME
-cd $PROJECT_NAME
+# Create a temporary directory for the project
+TEMP_DIR=$(mktemp -d)
 
 # Download the project's source code or distribution
-curl -L -o $PROJECT_NAME.zip $DOWNLOAD_URL
+curl -L -o $TEMP_DIR/$PROJECT_NAME.zip $DOWNLOAD_URL
 
-# Unzip the downloaded file (if it's a zip file)
-unzip -q $PROJECT_NAME.zip
+# Unzip the downloaded file to the temporary directory
+unzip -q $TEMP_DIR/$PROJECT_NAME.zip -d $TEMP_DIR
+
+# Find the directory containing your Python program
+PROGRAM_DIR=$(find $TEMP_DIR -type d -name "massplanner-agent*" | head -n 1)
+
+if [ -z "$PROGRAM_DIR" ]; then
+  echo "Error: Could not locate the program directory."
+  exit 1
+fi
+
+# Create a directory for the project and copy the contents
+mkdir -p $PROJECT_NAME
+cp -r $PROGRAM_DIR/* $PROJECT_NAME
 
 # Install pipenv for Python 3.9
 python3.9 -m pip install pipenv
+
+# Navigate to the project directory containing your Python program
+cd $PROJECT_NAME  # This should be the current directory now
 
 # Create a virtual environment for Python 3.9 and install project dependencies
 python3.9 -m pipenv --python 3.9 install
@@ -23,8 +37,11 @@ python3.9 -m pipenv --python 3.9 install
 # Activate the virtual environment
 python3.9 -m pipenv shell
 
-# Provide instructions for running the program
-echo "Installation complete. You can now run your program using 'python main.py'."
+# Install project dependencies (if any)
+pipenv install -r requirements.txt  # If you have a requirements.txt file
 
-# Exit the virtual environment when done
-exit
+# Provide instructions for running the program
+echo "Installation complete. You can now run your program using 'python massplanner-agent/instance.py"
+
+# Deactivate the virtual environment when done
+deactivate
